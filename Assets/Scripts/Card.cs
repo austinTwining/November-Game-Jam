a4a148b080984375ee8dfe760932e3350dfae5a0
Austin-James Twining <austin.twining@gmail.com>
2021-11-21 05:22:16 +0000

    added mouse input so card is flipped when clicked

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -19,19 +19,35 @@ public class Card : MonoBehaviour
     public CardType _type = CardType.Offensive;
     public Element _element = Element.Earth;
     public int damage = 0;
+    public int defense = 0;
     public int staminaCost = 0;
 
-    
+    public Transform transform;
+    public BoxCollider2D boxCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        transform = this.GetComponent<Transform>();
+        boxCollider = this.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider == boxCollider) {
+                flip();
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData){
+        Debug.Log(" Game Object Clicked!");
     }
 
     void flip(){

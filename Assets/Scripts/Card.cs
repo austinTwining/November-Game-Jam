97c81bdf05c87f3266a9da81e0363e82a8bf436d
Austin-James Twining <austin.twining@gmail.com>
2021-11-21 04:17:51 +0000

    initial card structure

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum CardType{
+    Offensive,
+    Defensive
+}
+public enum Element{
+    Earth,
+    Air,
+    Fire,
+    Water
+}
+
+public class Card : MonoBehaviour
+{
+    public CardType _type = CardType.Offensive;
+    public Element _element = Element.Earth;
+    public int damage = 0;
+    public int staminaCost = 0;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void flip(){
+        _type = _type == CardType.Offensive ? _type = CardType.Defensive : _type = CardType.Offensive;
+    }
+}
